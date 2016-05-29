2d8b39db0397015348f75d52a00d87c3e82b12dc
k1dave6412 <k1dave6412@gmail.com>
2016-05-29 21:12:24 +0800

    hw6666

--- HomeWork6 - 複製/HomeWork6/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int counterline(FILE *Ptr);
-void filter(FILE *Ptr, char **input, int times);
-void write(FILE *fPtr, char content[]);
-FILE* open(char fileName[], char type[]);
-void fill(FILE *Ptr, char ** filter, int times);
